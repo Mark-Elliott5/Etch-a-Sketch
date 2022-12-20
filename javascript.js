44776a3fa700235ca27039a53d1f816c418dee80
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2022-12-20 14:57:31 -0500

    Grid slider implemented, prompt removed

--- javascript.js
+++ javascript.js
@@ -1,26 +1,29 @@
 const body = document.querySelector('body');
-
 const gridWrapper = document.getElementById('grid-wrapper');
+const slider = document.getElementById('dimension-slider');
+slider.addEventListener('mouseup', createGrid);
+const clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', clearGrid);
 
-gridDimensions = parseInt(prompt());
-
-for (let i = 0; i < (gridDimensions**2); i++) {
-    let box = document.createElement('div');
-    box.classList.add('box');
-    box.addEventListener('mouseover', () => box.classList.add('gridColor'));
-    gridWrapper.appendChild(box);
+function createGrid() {
+    gridWrapper.replaceChildren();
+    const gridDimensions = slider.value;
+    for (let i = 0; i < (gridDimensions**2); i++) {
+        let box = document.createElement('div');
+        box.classList.add('box');
+        box.addEventListener('mouseover', () => box.classList.add('gridColor'));
+        gridWrapper.appendChild(box);
+    }
+    gridWrapper.style.cssText = 
+    `grid-template-columns:repeat(${gridDimensions}, minmax(0, 1fr));
+    grid-template-rows:repeat(${gridDimensions}, minmax(0, 1fr))`;
 }
 
-gridWrapper.style.cssText = 
-`grid-template-columns:repeat(${gridDimensions}, minmax(0, 1fr));
-grid-template-rows:repeat(${gridDimensions}, minmax(0, 1fr))`;
-
-const clearButton = document.getElementById('clear');
-clearButton.addEventListener('click', () => {
+function clearGrid() {
     const nodesToBeCleared = gridWrapper.children;
     for (let i = 0; i < nodesToBeCleared.length; i++) {
         nodesToBeCleared[i].classList.remove('gridColor');
         }
-    }
-);
+}
 
+createGrid();
