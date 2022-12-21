6ab6454e3221fc239f30003c2c927a9129490bb6
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2022-12-21 12:50:03 -0500

    Drawing only occurs while click is held down now

--- javascript.js
+++ javascript.js
@@ -1,9 +1,37 @@
-const body = document.querySelector('body');
 const gridWrapper = document.getElementById('grid-wrapper');
+
 const slider = document.getElementById('dimension-slider');
 slider.addEventListener('mouseup', createGrid);
+
 const clearButton = document.getElementById('clear');
 clearButton.addEventListener('click', clearGrid);
+
+let clickHeldDown = false;
+gridWrapper.addEventListener('mousedown', () => clickHeldDown = true);
+gridWrapper.addEventListener('mouseup', () => clickHeldDown = false);
+
+// const eraseButton = document.getElementById('erase');
+// eraseButton.addEventListener('click', () => );
+
+// const rainbowButton = document.getElementById('rainbow');
+// rainbowButton = document.addEventListener('change', rainbow);
+
+// function rainbow() {
+//     // const gridColorRule = document.styleSheets[0].cssRules.cssText;
+//     const colors = ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'purple', 'pink']
+//     for (let i = 0; i < (document.styleSheets[0].cssRules.length); i++) {
+//         if (document.styleSheets[0].cssRules[i].selectorText == '.gridColor') {
+//             // document.styleSheets[0].cssRules[i].cssText = `.gridColor { background-color = ${color}`
+//             var gridColorRule = document.styleSheets[0].cssRules[i].cssText;
+//             break;
+//         }
+//     }
+//     while (rainbowButton.checked) {
+//         run random color getter function;
+//         gridColorRule = random color;
+//         return;
+//     }
+// }
 
 function createGrid() {
     gridWrapper.replaceChildren();
@@ -11,7 +39,13 @@ function createGrid() {
     for (let i = 0; i < (gridDimensions**2); i++) {
         let box = document.createElement('div');
         box.classList.add('box');
-        box.addEventListener('mouseover', () => box.classList.add('gridColor'));
+        box.addEventListener('mouseover', () => {
+            if (clickHeldDown) {
+                box.classList.add('grid-color');
+            } else { 
+                return; 
+            }
+        })
         gridWrapper.appendChild(box);
     }
     gridWrapper.style.cssText = 
@@ -22,7 +56,7 @@ function createGrid() {
 function clearGrid() {
     const nodesToBeCleared = gridWrapper.children;
     for (let i = 0; i < nodesToBeCleared.length; i++) {
-        nodesToBeCleared[i].classList.remove('gridColor');
+        nodesToBeCleared[i].classList.remove('grid-color');
         }
 }
 
